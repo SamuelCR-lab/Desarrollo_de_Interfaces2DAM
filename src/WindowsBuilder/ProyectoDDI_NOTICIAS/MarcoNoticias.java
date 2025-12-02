cb913560f1cda1119c8e7489e5986b3ec99d6bbc
SamuelCR-lab <fp.scariasramos@salesianosdosa.com>
2025-12-02 14:28:55 +0100

    Panel se despliega en medio de la pantalla

--- src/WindowsBuilder/ProyectoDDI_NOTICIAS/MarcoNoticias.java
+++ src/WindowsBuilder/ProyectoDDI_NOTICIAS/MarcoNoticias.java
@@ -1,17 +1,21 @@
 package ProyectoDDI_NOTICIAS;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
-
-
 
 public class MarcoNoticias extends JFrame{
 	public MarcoNoticias () {
 		setTitle("Buscador Noticias");
-		setSize(800,600);
-		
-		setLocationRelativeTo(null);
+		setSize(1200,800);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Principal.class.getResource("/icono_noticias_globales.png")));
 		setResizable(false);
-	
+		Dimension monitor = Toolkit.getDefaultToolkit().getScreenSize();
+		int tamanioyH = (int) monitor.getHeight();
+		int tamanioxW = (int) monitor.getWidth();
+		setLocation((tamanioxW/2 - getWidth()/2), (tamanioyH/2 - getHeight()/2));
+		
 		Paneles misPaneles = new Paneles();
 		add(misPaneles);
 	}
